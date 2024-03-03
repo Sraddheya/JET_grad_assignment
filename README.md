6945e990f945c8fc3ab0cfaf68d7c504048a030d
Sraddheya <ggsraddheya@gmail.com>
2024-03-03 16:51:35 +0000

    added function to clean post code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JET_grad_assignment
+
 Coding assignment for the JET Graduate Scheme 2024
+UK postal code format: https://assets.publishing.service.gov.uk/media/5a81ebbded915d74e6234d42/Appendix_C_ILR_2017_to_2018_v1_Published_28April17.pdf
--- main.js
+++ main.js
@@ -9,6 +9,30 @@ function restaurant(name, rating, cuisines, city, firstLine, postalCode){
     this.postalCode = postalCode;
 }
 
+function cleanPostalCode(postalCode){
+    console.log(postalCode);
+    // Turn to upper case and remove non alphanumeric characters
+    newPostalCode = postalCode.toUpperCase().replace(/[^0-9a-z]/gi, '');
+    console.log(newPostalCode);
+    //Matches length
+    if (newPostalCode.length < 5 || newPostalCode > 7){return null;}
+    // Matches UK postal code pattern
+    regex1 = /[A-Z]{1,2}[0-9]{2,3}[A-Z]{2}/;
+    regex2 = /[A-Z]{1,2}[0-9][A-Z][0-9][A-Z]{2}/;
+    if (regex1.test(newPostalCode) || regex2.test(newPostalCode)){
+        return newPostalCode;
+    } else {
+        return none;
+    }
+}
+
+console.log(cleanPostalCode("EC4M7RF") === "EC4M7RF");
+console.log(cleanPostalCode("EC4M7rf") === "EC4M7RF");
+console.log(cleanPostalCode("EC4M 7RF") === "EC4M7RF");
+console.log(cleanPostalCode("EC4M7RF~!@#$%^&*()-_+=[]{}|;:'\",.<>?/`") === "EC4M7RF");
+console.log(cleanPostalCode("EC4M\n7RF") === "EC4M7RF");
+
+
 async function getData() {
     let rests = [];
 
@@ -35,4 +59,4 @@ async function getData() {
     }
 }
 
-getData();
+//getData();
