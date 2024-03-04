9d01ceb74050944f991410433d51dd8bb7aba1b8
Sraddheya <ggsraddheya@gmail.com>
2024-03-04 21:13:43 +0000

    refactored and improved output format

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 const ENDPOINT = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/";
 const NUMTOPRINT = 10;
 
+// Class to store restaurant data as an object
 class restaurant {
-    constructor(name = null, rating = null, cuisines = null, city = null, firstLine = null, postalCode = null) {
+    constructor(name = null, rating = null, cuisines = null, city = null, firstLine = null, postCode = null) {
         this.name = name;
         this.rating = rating ;
         this.cuisines = cuisines;
         this.city = city;
         this.firstLine = firstLine;
-        this.postalCode = postalCode;
+        this.postCode = postCode;
     }
 }
 
-function getCleanPostalCode(postalCode){
-    return postalCode.toUpperCase().replace(/[^0-9a-z]/gi, '');
+function getCleanPostCode(postCode){
+    return postCode.toUpperCase().replace(/[^0-9a-z]/gi, '');
 }
 
-function isValidPostCode(postalCode){
+function isValidPostCode(postCode){
+    // Valid UK postcode formats as regular expressions
     regex1 = /[A-Z]{1}[0-9]{2}[A-Z]{2}/;
     regex2 = /[A-Z]{1}[0-9]{3}[A-Z]{2}/;
     regex3 = /[A-Z]{2}[0-9]{2}[A-Z]{2}/;
@@ -24,7 +26,7 @@ function isValidPostCode(postalCode){
     regex5 = /[A-Z]{1}[0-9]{1}[A-Z]{1}[0-9]{1}[A-Z]{2}/;
     regex6 = /[A-Z]{2}[0-9]{1}[A-Z]{1}[0-9]{1}[A-Z]{2}/;
 
-    if (regex1.test(postalCode) || regex2.test(postalCode) || regex3.test(postalCode) || regex4.test(postalCode) || regex5.test(postalCode) || regex6.test(postalCode)){
+    if (regex1.test(postCode) || regex2.test(postCode) || regex3.test(postCode) || regex4.test(postCode) || regex5.test(postCode) || regex6.test(postCode)){
         return true;
     } else {
         return false;
@@ -32,13 +34,13 @@ function isValidPostCode(postalCode){
 }
 
 async function getData(postalCode) {
-    let rests = [];
+    let restaurantData = [];
 
     try {
         // Fetch data from API endpoint
         const response = await fetch(ENDPOINT + encodeURIComponent(postalCode));
         if (!response.ok){
-            throw new Error(`HTTP error! Status: ${response.status}`);
+            throw new Error(`HTTP error status ${response.status}`);
         }
         
         // Extract desired data in json form
@@ -47,30 +49,28 @@ async function getData(postalCode) {
             throw new Error(`There is no restaurant data for this post code.`);
         }
         for (const r of data.restaurants){
-            let cui = [];
+            let cuisinesArray = [];
             for (const c of r.cuisines){
-                cui.push(c.name);
+                cuisinesArray.push(c.name);
             }
-            const s = new restaurant(r.name, r.rating.starRating, cui, r.address.city, r.address.firstLine, r.address.postalCode);
-            rests.push(s);
+            const restaurantObject = new restaurant(r.name, r.rating.starRating, cuisinesArray, r.address.city, r.address.firstLine, r.address.postalCode);
+            restaurantData.push(restaurantObject);
         }
 
         // Output first x number of resturants
-        printLimit = Math.min(NUMTOPRINT, rests.length);
-        if (rests.length < NUMTOPRINT){
-            console.log(`There are only ${rests.length} restaurants in this area`);
+        printLimit = Math.min(NUMTOPRINT, restaurantData.length);
+        if (restaurantData.length < NUMTOPRINT){
+            console.log(`There are only ${restaurantData.length} restaurants in this area`);
         }
         for (let i = 0; i < printLimit; i ++){
-            console.log(`\nRestaurant ${i + 1}`);
-            console.log(`Name: ${rests[i].name}`);
-            console.log(`Rating: ${rests[i].rating}`);
-            console.log(`Cuisines: ${rests[i].cuisines}`);
-            console.log(`Address: ${rests[i].firstLine}, ${rests[i].city}, ${rests[i].postalCode}`);
-            //console.log(rests[i]);
+            console.log(`\n---RESTAURANT ${i + 1}---`);
+            console.log(`Name: ${restaurantData[i].name}`);
+            console.log(`Rating: ${restaurantData[i].rating}`);
+            console.log(`Cuisines: ${restaurantData[i].cuisines}`);
+            console.log(`Address: ${restaurantData[i].firstLine}, ${restaurantData[i].city}, ${restaurantData[i].postCode}`);
         }
-
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('ERROR FETCHING DATA:', error);
     }
 }
 
@@ -80,16 +80,17 @@ const readline = require('readline').createInterface({
 });
 
 async function processInput() {
-    readline.question('Enter a valid UK post code or type "exit" to quite: ', async (input) => {
+    console.log("\nINPUT REQUEST ------------------------------------------")
+    readline.question('Enter a UK post code or type "exit" to quite: ', async (input) => {
         if (input.toLowerCase() === 'exit') {
             readline.close();
         } else {
-            postCode = getCleanPostalCode(input);
+            postCode = getCleanPostCode(input);
             if (isValidPostCode(postCode)) {
                 await getData(postCode);
                 await processInput();
             } else {
-                console.error('Invalid input. Please try again.');
+                console.error('ERROR GETTING POST CODE: Invalid UK post code. Please try again.');
                 await processInput();
             }
         }
